f2bf52ec26d292e894af480d2cffc37e7a3ca7f4
unknown <rouchashoori@gmail.com>
2024-06-27 22:05:42 +0330

    validating user payload and added logic for creating user instance before saving it to database

--- types.go
+++ types.go
@@ -1,16 +1,56 @@
 package main
 
+import (
+	"net/http"
+	"time"
+)
 
+// our custom error struct
 type Error struct {
-	Error 	string	`json:"error"`
-	Code	int		`json:"code"`
+	Message string `json:"error"`
+	Code  int    `json:"code"`
 }
 
+// wrote a method for our new custom error struct so we can use it for returning errors of functions
+func (e *Error) Error() string {
+	return e.Message
+}
 
 type User struct {
-	ID			int		`json:"id"`
-	Username 	string	`json:"username"`
-	Password	string	`json:"password"`
+	ID        int    	`json:"id" db:"id"`
+	Username  string 	`json:"username" db:"username"`
+	Password  string 	`json:"password" db:"password"`
+	CreatedAt time.Time	`json:"createdAt" db:"created_at"`
 }
 
+type UserRequest struct {
+	Username  string 	`json:"username" db:"username"`
+	Password  string 	`json:"password" db:"password"`
+	CreatedAt time.Time	`json:"createdAt" db:"created_at"`
+}
 
+// a method for creating an instancee of user before saving it to database
+func (u *UserRequest) CreateUser() (*UserRequest, *Error) {
+	// checks if the request for creating user has requried fields
+	if u.Username == "" || u.Password == "" {
+		return nil , &Error{
+			Message: "username and password fields are required.", 
+			Code: http.StatusBadRequest,
+		}
+	}
+
+	if len(u.Username) < 8 || len(u.Password) < 8 {
+		return nil , &Error{
+			Message: "username and password should be bigger than 8 characters.",
+			Code: http.StatusBadRequest,
+		}
+	}
+
+	user := &UserRequest{
+		Username: u.Username, 
+		Password: u.Password,
+		CreatedAt: time.Now().UTC(),
+	}
+
+	return user , nil
+}
