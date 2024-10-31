db2c2f216d0b472331f88b96ce766f21557f0f3b
unknown <rouchashoori@gmail.com>
2024-10-31 21:05:02 +0330

    intialized modules

--- main.go
+++ main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"github.com/Rouch3362/url-shortener/api"
+	"github.com/Rouch3362/url-shortener/cmd"
+)
 
 // main function to run the whole app
 func main() {
-	fmt.Println("Server is Running on port 8000")
+	storage , err := cmd.ConnectionToDB()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = storage.InitDB()
+	
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	api.Run()
 }
