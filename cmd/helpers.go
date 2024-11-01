2c83c5311a35838da284e3671104575ed3aaacc3
unknown <rouchashoori@gmail.com>
2024-11-01 21:16:13 +0330

    created functionalities for creating JWT tokens

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-
+	"time"
+	"github.com/Rouch3362/url-shortener/types"
+	"github.com/golang-jwt/jwt"
 	"github.com/joho/godotenv"
 )
 
@@ -32,4 +34,46 @@ func JsonGenerator(w http.ResponseWriter, statusCode int, value any) {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+
+
+func GenerateJWTToken(payload *types.UserResponse, isAccessToken bool) string {
+	// expires at 12 hours later of generation
+	expHour := time.Hour * 12 
+
+	// the default type of token is access token
+	tokenType := "access"
+
+	// changes based on argument entered
+	if !isAccessToken {
+		tokenType = "refresh"
+		expHour *= 2
+	}
+
+	// payload of token
+	claims := jwt.MapClaims{
+		"id": payload.Id,
+		"username": payload.Username,
+		"exp": time.Now().Add(expHour).Unix(),
+		"type": tokenType,
+	}
+
+	// generating token 
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	// getting secret of tokens
+	jwtSecret := ReadEnvVar("JWT_SECRET")
+
+
+	// extracting token string from token
+	tokenString, err := token.SignedString([]byte(jwtSecret))
+
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+	return tokenString
 }
