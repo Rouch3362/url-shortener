32b738b8d258f6a2ec1d58974050f72db2680a60
unknown <rouchashoori@gmail.com>
2024-11-01 21:12:36 +0330

    added routes for login and user registeration (will change in future)

--- api/main.go
+++ api/main.go
@@ -3,15 +3,24 @@ package api
 import (
 	"fmt"
 	"net/http"
-
+	"github.com/Rouch3362/url-shortener/cmd/db"
 	"github.com/gorilla/mux"
 )
 
-func Run() {
+// an API server struct for accessing storage object and running its methods
+type APIServer struct {
+	Addr 	string
+	DB		*db.Storage
+}
+
+// a method for APIServer to run the server
+func (a *APIServer) Run() {
 	router := mux.NewRouter()
 
-	router.HandleFunc("/urls" , createUrlsHandler).Methods("POST")
+	router.HandleFunc("/urls" , a.createUrlsHandler).Methods("POST")
+	router.HandleFunc("/users", a.createUserHandler).Methods("POST")
+	router.HandleFunc("/login", a.LoginHandler).Methods("POST")
 
 	fmt.Println("Server is Running on port 8000")
-	http.ListenAndServe(":8000", router)
+	http.ListenAndServe(a.Addr, router)
 }
