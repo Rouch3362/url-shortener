43a0eb9d995e46ffd15c1c4e865847e90457aed6
unknown <rouchashoori@gmail.com>
2024-11-01 21:16:45 +0330

    created user response struct

--- types/response.go
+++ types/response.go
@@ -5,3 +5,11 @@ package types
 type ErrorMessage struct {
 	Message   	string `json:"message"`
 }
+
+
+// a struct for showing user columns
+type UserResponse struct {
+	Id 			int	   `json:"id"`
+	Username	string `json:"username"`
+	CreatedAt 	string	`json:"created_at"`
+}
