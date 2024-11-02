a45d7fb2e5b66cff790fcaa868a843b7e8f76225
unknown <rouchashoori@gmail.com>
2024-11-02 22:24:55 +0330

    created method for token field validation and some struct

--- types/tokens.go
+++ types/tokens.go
@@ -6,3 +6,23 @@ type Token struct {
 	AcccessToken 	string `json:"access_token"`
 	RefreshToken 	string `json:"refresh_token"`	
 }
+
+
+type RefreshTokenRequest struct {
+	RefreshToken 	string  `json:"refresh_token"`
+}
+
+type TokenDBRequest struct {
+	AccessToken 	string
+	RefreshToken 	string
+	ExpiresAt		int64
+	UserId 			int
+}
+
+func (r *RefreshTokenRequest) Validate() string{
+	if r.RefreshToken == "" {
+		return "refresh_token field is required."
+	}
+
+	return ""
+}
