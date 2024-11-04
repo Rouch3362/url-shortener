66fa6d98d505565f5c0c77703737ef7b5854caed
unknown <rouchashoori@gmail.com>
2024-11-04 22:59:51 +0330

    added route for getting user's urls

--- api/main.go
+++ api/main.go
@@ -3,7 +3,9 @@ package api
 import (
 	"fmt"
 	"net/http"
+
 	"github.com/Rouch3362/url-shortener/cmd/db"
+	"github.com/gorilla/mux"
 )
 
 // an API server struct for accessing storage object and running its methods
@@ -14,16 +16,19 @@ type APIServer struct {
 
 // a method for APIServer to run the server
 func (a *APIServer) Run() {
-	router := http.NewServeMux()
+	router := mux.NewRouter()
 	
 	
 	createUrlRoute := http.HandlerFunc(a.createUrlsHandler)
-	router.Handle("/urls" , AuthMiddleware(createUrlRoute))
-	router.HandleFunc("/users", a.createUserHandler)
-	router.HandleFunc("/login", a.LoginHandler)
+	router.Handle("/urls", AuthMiddleware(createUrlRoute)).Methods("POST")
+	router.HandleFunc("/{id}", a.getUrlHandler).Methods("GET")
+	router.HandleFunc("/users", a.createUserHandler).Methods("POST")
+	router.HandleFunc("/users/{username}", a.GetUserURLs).Methods("GET")
+	// router.HandleFunc("/whoami", a.).Methods("GET")
+	router.HandleFunc("/login", a.LoginHandler).Methods("POST")
 
 	refreshTokenRoute := http.HandlerFunc(a.RefreshTokenHandler)
-	router.Handle("/refresh-token", AuthMiddleware(refreshTokenRoute))
+	router.Handle("/refresh-token", AuthMiddleware(refreshTokenRoute)).Methods("POST")
 
 	fmt.Println("Server is Running on port 8000")
 	http.ListenAndServe(a.Addr, router)
